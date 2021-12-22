6150a81e7d0ae6ba53b378a61d12866892b224cc
Eli Lisseck <eli@aghstrategies.com>
2021-12-21 20:27:28 -0800

    change to upload button type

--- js/confirmbuttons.js
+++ js/confirmbuttons.js
@@ -1,3 +1,4 @@
 CRM.$(function($) {
   $('button.crm-form-submit.crm-button-type-next').text('Confirm Membership');
+  $('button.crm-form-submit.crm-button-type-upload').text('Confirm Membership');
 });
--- js/reviewbuttons.js
+++ js/reviewbuttons.js
@@ -1,5 +1,6 @@
 CRM.$(function($) {
   $('button.crm-form-submit.crm-button-type-next').text('Review Membership');
+  $('button.crm-form-submit.crm-button-type-upload').text('Review Membership');
   $('#editrow-custom_19').hide();
   $('#custom_18_Other').click(function(){
     $('#editrow-custom_19').toggle();
