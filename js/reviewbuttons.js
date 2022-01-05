afdbac8f04fd61f8b5a5a7db1ba98bf0ebf66322
Eli Lisseck <eli@aghstrategies.com>
2022-01-04 17:22:38 -0800

    change review text to create text

--- js/reviewbuttons.js
+++ js/reviewbuttons.js
@@ -1,6 +1,6 @@
 CRM.$(function($) {
-  $('button.crm-form-submit.crm-button-type-next').text('Review Membership');
-  $('button.crm-form-submit.crm-button-type-upload').text('Review Membership');
+  $('button.crm-form-submit.crm-button-type-next').text('Create Membership');
+  $('button.crm-form-submit.crm-button-type-upload').text('Create Membership');
   $('#editrow-custom_19').hide();
   $('#custom_18_Other').click(function(){
     $('#editrow-custom_19').toggle();
